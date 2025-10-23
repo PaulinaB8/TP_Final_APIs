51d9bc55dc186e147252bb028d70d55f46a88546
janaslavin <janaslavin@gmail.com>
2025-10-23 14:07:02 -0300

    userRepository

--- TP_Final_APIs/Repositories/Implementations/UserRepository.cs
+++ TP_Final_APIs/Repositories/Implementations/UserRepository.cs
@@ -1,6 +1,41 @@
-﻿namespace TP_Final_APIs.Repositories.Implementations
+﻿using TP_Final_APIs.Entities;
+using TP_Final_APIs.Repositories.Interfaces;
+
+namespace TP_Final_APIs.Repositories.Implementations
 {
-    public class UserRepository
+    public class UserRepository : IUserRepository
     {
+        public List<User> _context = new List<User>();
+
+
+        public void createRestaurant(User newRestaurant)
+        {
+            _context.Add(newRestaurant);
+            //_context: SaveChanges();
+            
+        }
+
+        public void deleteUser(int idUser)
+        {
+            var userDeleted = _context.FirstOrDefault(u => u.Id == idUser);
+        }
+
+        public IEnumerable<User> getAllRestaurants()
+        {
+            return _context;
+        }
+
+        
+
+        public void updateUser(User updatedUser, int idUser)
+        {
+            //User? user = _context.SingleOrDefault(user => user.Id == idUser);
+
+            //if ( user is not null)
+            //{
+                
+            //}
+            throw new NotImplementedException();
+        }
     }
 }
--- TP_Final_APIs/Repositories/Interfaces/IUserRepository.cs
+++ TP_Final_APIs/Repositories/Interfaces/IUserRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace TP_Final_APIs.Repositories.Interfaces
+﻿using TP_Final_APIs.Entities;
+
+namespace TP_Final_APIs.Repositories.Interfaces
 {
     public interface IUserRepository
     {
+        IEnumerable<User> getAllRestaurants();
+        void createRestaurant(User newRestaurant);
+        void deleteUser(int idUser);
+        void updateUser(User updatedUser, int idUser);
     }
 }
