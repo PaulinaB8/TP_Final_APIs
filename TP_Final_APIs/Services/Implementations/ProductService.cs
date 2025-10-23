73381e3553ed2505638506621c452a35ee538eeb
PaulinaB8 <paulinabruno343@gmail.com>
2025-10-23 14:42:53 -0300

    inyección de repositorio en ProductService

--- TP_Final_APIs/Services/Implementations/ProductService.cs
+++ TP_Final_APIs/Services/Implementations/ProductService.cs
@@ -1,6 +1,57 @@
-﻿namespace TP_Final_APIs.Services.Implementations
+﻿using TP_Final_APIs.Models.DTOs.Requests;
+using TP_Final_APIs.Models.DTOs.Responses;
+using TP_Final_APIs.Repositories.Interfaces;
+using TP_Final_APIs.Services.Interfaces;
+
+namespace TP_Final_APIs.Services.Implementations
 {
-    public class ProductService
+    public class ProductService :IProductService
     {
+        public ProductService(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+        private readonly IProductRepository _productRepository;
+        public IEnumerable<ProductDto> getProductsByCategory(int idCategory)
+        {
+
+        }
+        public ProductDto getProduct(int idProduct)
+        {
+
+        }
+        public IEnumerable<ProductDto> getFavouriteProducts()
+        {
+
+        }
+        public IEnumerable<ProductDto> getDiscountProducts()
+        {
+
+        }
+        public IEnumerable<ProductDto> getHappyHourProducts()
+        {
+
+        }
+
+        public void createProduct(CreateAndUpdateProductDto newProduct, int idCategory)
+        {
+
+        }
+        public void deleteProduct(int idProduct)
+        {
+
+        }
+        public void updateProduct(CreateAndUpdateProductDto updatedProduct, int idProduct)
+        {
+
+        }
+        public void changeDiscount(double discount)
+        {
+
+        }
+        public string applyHappyHour(int idProduct)
+        {
+
+        }
     }
 }
