07d9668e921ec63d7d6b7845dae4a5a651d63a83
cgorvalan <gorvalancandela@gmail.com>
2025-10-23 14:02:30 -0300

    implementaciones del categoryservice

--- TP_Final_APIs/Repositories/Implementations/CategoryRepository.cs
+++ TP_Final_APIs/Repositories/Implementations/CategoryRepository.cs
@@ -1,6 +1,36 @@
-﻿namespace TP_Final_APIs.Repositories.Implementations
+﻿using TP_Final_APIs.Entities;
+using TP_Final_APIs.Repositories.Interfaces;
+using TP_Final_APIs.Services.Interfaces;
+
+namespace TP_Final_APIs.Repositories.Implementations
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
+
     {
+        List<Category> _context = new List<Category>();
+        public void createCategory(Category newCategory)
+        {
+            _context.Add(newCategory);
+        }
+
+        public void deleteCategory(int idCategory)
+        {
+            var categoryToDelete = _context.FirstOrDefault(c => c.Id == idCategory);
+            if (categoryToDelete != null)
+            {
+                _context.Remove(categoryToDelete);
+            }
+        }
+
+        public IEnumerable<Category> getCategory(int idUser)
+        {
+            return _context.Where(c => c.Id == idUser);
+        }
+
+        public void updateCategory(Category updatedCategory, int idCategory)
+        {
+            
+
+        }
     }
 }
--- TP_Final_APIs/Repositories/Interfaces/ICategoryRepository.cs
+++ TP_Final_APIs/Repositories/Interfaces/ICategoryRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace TP_Final_APIs.Repositories.Interfaces
+﻿using TP_Final_APIs.Entities;
+
+namespace TP_Final_APIs.Repositories.Interfaces
 {
     public interface ICategoryRepository
     {
+        IEnumerable<Category> getMenu(int idRestaurant);
+        void updateCategory(Category updatedCategory, int idCategory);
+        void deleteCategory(int idCategory);
+        void createCategory(Category newCategory);
+
     }
 }
