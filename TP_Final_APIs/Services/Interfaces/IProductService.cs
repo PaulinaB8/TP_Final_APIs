1eaed66e062f8b18da70b8d58a81a8cac69448dc
cgorvalan <gorvalancandela@gmail.com>
2025-10-23 14:53:50 -0300

    Agrego implemntaciones en services

--- TP_Final_APIs/Services/Interfaces/IProductService.cs
+++ TP_Final_APIs/Services/Interfaces/IProductService.cs
@@ -1,6 +1,20 @@
-﻿namespace TP_Final_APIs.Services.Interfaces
+﻿using TP_Final_APIs.Models.DTOs.Requests;
+using TP_Final_APIs.Models.DTOs.Responses;
+
+namespace TP_Final_APIs.Services.Interfaces
 {
     public interface IProductService
     {
+        public IEnumerable<ProductDto> getProductsByCategory(int idCategory);
+        public ProductDto getProduct(int idProduct);
+        public IEnumerable<ProductDto> getFavouriteProducts();
+        public IEnumerable<ProductDto> getDiscountProducts();
+        public IEnumerable<ProductDto> getHappyHourProducts();
+
+        public void createProduct(CreateAndUpdateProductDto newProduct, int idCategory);
+        public void deleteProduct(int idProduct);
+        public void updateProduct(CreateAndUpdateProductDto updatedProduct, int idProduct);
+        public void changeDiscount(double discount);
+        public string applyHappyHour(int idProduct);
     }
 }
