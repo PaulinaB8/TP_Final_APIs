335fb462a2feecc938c80222e4329f536e0963f0
PaulinaB8 <paulinabruno343@gmail.com>
2025-10-23 13:50:05 -0300

    Interface e implementaciones de ProductRepository

--- TP_Final_APIs/Repositories/Interfaces/IProductRepository.cs
+++ TP_Final_APIs/Repositories/Interfaces/IProductRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace TP_Final_APIs.Repositories.Interfaces
+﻿using TP_Final_APIs.Entities;
+
+namespace TP_Final_APIs.Repositories.Interfaces
 {
     public interface IProductRepository
     {
+        public IEnumerable<Product> getProductsByCategory(int idCategory);
+        public Product getProduct(int idProduct);
+        public IEnumerable<Product> getFavouriteProducts();
+        public IEnumerable<Product> getDiscountProducts();
+        public IEnumerable<Product> getHappyHourProducts();
     }
 }
