a81f5b371879d13f6cc0d2c490d7e150781e578a
PaulinaB8 <paulinabruno343@gmail.com>
2025-10-23 14:34:38 -0300

    cambio en entidades

--- TP_Final_APIs/Entities/Category.cs
+++ TP_Final_APIs/Entities/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace TP_Final_APIs.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TP_Final_APIs.Entities
 {
     public class Category
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
-        public IEnumerable<Product> products { get; set; }
+        public IEnumerable<Product> Products { get; set; }
     }
 }
