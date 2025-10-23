b7bd2af06e5c42cfaf1da7c6fd4652eadab2e207
PaulinaB8 <paulinabruno343@gmail.com>
2025-10-23 13:26:09 -0300

    Agregado de propiedades en la entidad User

--- TP_Final_APIs/Entities/User.cs
+++ TP_Final_APIs/Entities/User.cs
@@ -1,6 +1,12 @@
-﻿namespace TP_Final_APIs.Entities
+﻿namespace TP_Final_APIs.Entities;
+
+public class User
 {
-    public class User
-    {
-    }
+    public int id {  get; set; }
+    public string name { get; set; }
+    public string password {  get; set; }
+    public string mail {  get; set; }
+    public bool status { get; set; }
+    public string phone {  get; set; }
+    public int idRestaurant { get; set; }
 }
