8d863fda0e1a81ff10b6065375cc960a3700636f
janaslavin <janaslavin@gmail.com>
2025-11-03 13:01:54 -0300

    base de datos migrations

---
+++ TP_Final_APIs/Migrations/20251103151550_Segunda migracion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TP_Final_APIs.Migrations
+{
+    /// <inheritdoc />
+    public partial class Segundamigracion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Products",
+                table: "Categories",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "[]");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Products",
+                table: "Categories");
+        }
+    }
+}
