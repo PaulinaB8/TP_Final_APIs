b71fa5f04afd9a086dcd019cf1b55648cbde21d9
PaulinaB8 <paulinabruno343@gmail.com>
2025-11-15 10:19:13 -0300

    Cambios en user create

--- TP_Final_APIs/Controllers/UsersController.cs
+++ TP_Final_APIs/Controllers/UsersController.cs
@@ -27,7 +27,7 @@ namespace TP_Final_APIs.Controllers
         }
 
         [HttpPost]
-        //[AllowAnonymous]
+        [AllowAnonymous]
         public IActionResult CreateRestaurant([FromBody]CreateUserDto userDto)
         {
             if (userDto == null)
